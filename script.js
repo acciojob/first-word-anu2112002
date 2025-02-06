4fc077d39910c6359ac20fe60682129f7fdf24fe
Anushri <112797477+anu2112002@users.noreply.github.com>
2025-02-06 11:44:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 function firstWord(s) {
   // your code here
+	str = str.trim();
+    let spaceIndex = str.indexOf(" ");
+    if (spaceIndex === -1) {
+        return str;
+    }
+    return str.substring(0, spaceIndex);
 }
-
 // Do not change the code below
 
 const s = prompt("Enter String:");
